0244a2bd2a708eb9087884ee2f938dcb52f7af9b
Jan Lauinger <lauinger.jan[at]googlemail.com>
2013-11-08 09:50:14 +0100

    Mein ertser Commit mit einer Testklasse aus Benutzer

---
+++ myFriend/src/main/java/de/myFriend/Benutzerverwaltung/domain/package-info.java
@@ -0,0 +1,11 @@
+@XmlAccessorType(FIELD)
+@Vetoed
+package de.myFriend.Benutzerverwaltung.domain;
+
+/**
+ * @author <a href="mailto:laja1011@HS-Karlsruhe.de">Jan Lauinger</a>
+ */
+import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+
+import javax.enterprise.inject.Vetoed;
+import javax.xml.bind.annotation.XmlAccessorType;
---
+++ myFriend/src/main/java/de/myFriend/Benutzerverwaltung/service/BenutzerService.java
@@ -0,0 +1,11 @@
+package de.myFriend.Benutzerverwaltung.service;
+
+import java.io.Serializable;
+
+/**
+ * @author <a href="mailto:laja1011@HS-Karlsruhe.de">Jan Lauinger</a>
+ */
+public class BenutzerService implements Serializable {
+	private static final long serialVersionUID = 5292529185811096603L;
+
+}
